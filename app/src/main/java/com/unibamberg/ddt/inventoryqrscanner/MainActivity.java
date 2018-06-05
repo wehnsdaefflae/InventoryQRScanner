d1b2b1fab304423519d0681a126316b965010d78
Mark <Mark@PC0U9AZF.UNI-BAMBERG.DE>
2018-06-05 16:51:29 +0200

    Initial commit

---
+++ app/src/main/java/com/unibamberg/ddt/inventoryqrscanner/MainActivity.java
@@ -0,0 +1,54 @@
+package com.unibamberg.ddt.inventoryqrscanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Activity activity = this;
+        Button scanBtn = findViewById(R.id.scan_btn);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setBeepEnabled(true);
+                integrator.setCameraId(0);
+                //integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String scannedData = result.getContents();
+
+            if (scannedData != null) {
+                new SendRequest().execute(scannedData);
+
+            } else {
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
---
+++ app/src/main/java/com/unibamberg/ddt/inventoryqrscanner/SendRequest.java
@@ -0,0 +1,104 @@
+package com.unibamberg.ddt.inventoryqrscanner;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SendRequest extends AsyncTask<String, Void, String> {
+    protected void onPreExecute(){}
+
+    @Override
+    protected String doInBackground(String... scannedData) {
+        String returnString;
+
+        try {
+            URL url = new URL("https://script.google.com/macros/s/AKfycbwxMET5gAMQ5HdS8OWJok8cT4M9THzAsyieWZdM/exec");
+
+            String data = scannedData[0];
+            if (!data.startsWith("(uniba-iadk)")) {
+                returnString = "invalid signature";
+
+            } else {
+                String json = String.format("{\"sdata\": \"%s\"}", data.substring(12));
+                JSONObject postDataParams = new JSONObject(json);
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    for (String line = in.readLine(); line != null; line = in.readLine()) {
+                        sb.append(line);
+                    }
+                    in.close();
+
+                    returnString = sb.toString();
+
+                } else {
+                    returnString = "http response not ok: " + responseCode;
+                }
+            }
+
+        } catch(Exception e){
+            returnString = "Exception: " + e.getMessage();
+        }
+
+        return returnString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+    }
+
+    private String getPostDataString(JSONObject params) throws Exception {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first) { first = false; }
+            else { result.append("&"); }
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+
+}
